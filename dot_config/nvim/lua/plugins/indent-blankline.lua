cb9f5621a53433fbf76b5d130c299887b624edf5
mango <mangogit@protonmail.com>
2023-10-09 23:37:05 -0400

    nvim: add indent-blankline v3 config

--- dot_config/nvim/lua/plugins/indent-blankline.lua
+++ dot_config/nvim/lua/plugins/indent-blankline.lua
@@ -1,18 +1,37 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
-    opts = {
-        max_indent_increase = 1,
-        filetype_exclude = {
-            "sagafinder",
-            "markdown",
-            "txt",
-            "lspinfo",
-            "packer",
-            "checkhealth",
-            "help",
-            "man",
-            "trouble",
-            "",
-        },
-    }
+    dependencies = "HiPhish/rainbow-delimiters.nvim",
+    config = function ()
+        require "ibl".setup({
+            scope = {
+                show_start = false,
+                show_end = false,
+            },
+            exclude = {
+                filetypes = {
+                    "markdown",
+                    "txt",
+                    "lazy",
+                }
+            }
+        })
+
+        local highlight = {
+            "RainbowDelimiterRed",
+            "RainbowDelimiterYellow",
+            "RainbowDelimiterBlue",
+            "RainbowDelimiterOrange",
+            "RainbowDelimiterGreen",
+            "RainbowDelimiterViolet",
+            "RainbowDelimiterCyan",
+        }
+
+        require "ibl".update { scope = { highlight = highlight } }
+
+        local hooks = require "ibl.hooks"
+        hooks.register(
+            hooks.type.SCOPE_HIGHLIGHT,
+            hooks.builtin.scope_highlight_from_extmark
+        )
+    end
 }
