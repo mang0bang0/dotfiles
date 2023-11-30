b08e542b4a9e0509d6a1412e065eedcfcc759e7b
mango <mangogit@protonmail.com>
2023-11-30 00:08:36 -0500

    nvim: auto trim trailing whitespace with autocmd

--- dot_config/nvim/lua/plugins/mini-trailspace.lua
+++ dot_config/nvim/lua/plugins/mini-trailspace.lua
@@ -1,5 +1,15 @@
 return {
     "echasnovski/mini.trailspace",
     version = '*',
-    config = true,
+    config = function ()
+        require("mini.trailspace").setup()
+
+        -- Set up autocmd to remove trailing whitespace and empty lines on save
+        vim.api.nvim_create_autocmd({"BufWritePost"}, {
+            callback = function ()
+                MiniTrailspace.trim()
+                MiniTrailspace.trim_last_lines()
+            end
+        })
+    end
 }
