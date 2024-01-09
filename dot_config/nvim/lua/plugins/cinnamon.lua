efb75beaa138cf04f3de42478fd185f93fae7110
mango <mangogit@protonmail.com>
2024-01-09 17:50:01 -0500

    nvim: reduce scroll time

--- dot_config/nvim/lua/plugins/cinnamon.lua
+++ dot_config/nvim/lua/plugins/cinnamon.lua
@@ -5,7 +5,7 @@ return {
         extended_keymaps = true,
         hide_cursor = true,
         max_length = 150,
-        scroll_limit = 75,
+        scroll_limit = 50,
         horizontal_scroll = false,
     }
 }
