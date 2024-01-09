d678dc7bfeeee1195e4fc77e300e24213223911e
mango <mangogit@protonmail.com>
2024-01-09 17:49:52 -0500

    nvim: update aerial settings

---
+++ dot_config/nvim/lua/plugins/aerial.lua
@@ -0,0 +1,17 @@
+return {
+    "stevearc/aerial.nvim",
+    opts = {
+        layout = {
+            min_width = 0.15
+        },
+
+        keymaps = {
+            ["v"] = "actions.jump_vsplit",
+            ["h"] = "actions.jump_split",
+        },
+
+        highlight_on_jump = false,
+
+        show_guides = true,
+    }
+}
