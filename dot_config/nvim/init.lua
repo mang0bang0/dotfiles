6033f44560e7d9dff31b1bae1dbf9da2cfb85246
mango <mangogit@protonmail.com>
2024-01-28 22:23:49 -0500

    nvim: fix lua_ls taking forever

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -300,9 +300,10 @@ require("lspconfig").lua_ls.setup{
                 globals = {'vim'},
             },
             workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
                 checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME
+                }
             },
         },
     },
