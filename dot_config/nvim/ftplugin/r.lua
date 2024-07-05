2a78850d7ed3f173f1ca579afa89e0c4535c8bd5
mango <mangogit@protonmail.com>
2024-07-04 22:08:05 -0400

    nvim: add r and text ftplugins

---
+++ dot_config/nvim/ftplugin/r.lua
@@ -0,0 +1,9 @@
+--2 space indentations
+vim.opt.shiftwidth = 2
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 0
+
+-- have comments continue on hitting enter in insert mode
+vim.opt.formatoptions:append("r")
