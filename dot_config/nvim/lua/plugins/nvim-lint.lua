fc7f738d864683fa52c875fa671a4ab7bd51537c
mango <mangogit@protonmail.com>
2024-08-28 17:53:52 -0400

    nvim: add codespell to c++ files

--- dot_config/nvim/lua/plugins/nvim-lint.lua
+++ dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -4,6 +4,8 @@ return {
     config = function ()
         require("lint").linters_by_ft = {
             fish = {"fish"},
+            cpp = {"codespell"},
+            hpp = {"codespell"},
         }
 
         vim.api.nvim_create_autocmd({"TextChanged"}, {
